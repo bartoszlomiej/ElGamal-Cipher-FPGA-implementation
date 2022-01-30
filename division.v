ac4f463dd28374433b1ddfdd0e48bd26289697c9
bartoszlomiej <jet.samb@gmail.com>
2022-01-30 15:18:10 +0100

    Division applied properly

---
+++ division.v
@@ -0,0 +1,92 @@
+`timescale 1ns / 1ps
+
+module division #(parameter SIZE = 64)
+   (
+    input wire 		     clk, rst,
+    // using AXI stream inputs
+    wire [(SIZE*2)-1 : 0]    input_dividen_tdata,
+    input wire 		     input_dividen_tvalid,
+    output wire 	     input_dividen_tready,
+   
+    input wire [SIZE-1 : 0]  input_divisor_tdata,
+    input wire 		     input_divisor_tvalid, 
+    output wire 	     input_divisor_tready,
+    // using AXI stream outputs
+    output wire [(SIZE*2)-1 : 0] output_tdata,
+    output wire 	     output_tvalid, 
+    input wire 		     output_tready
+    );
+   
+   reg [(SIZE*2)-1 : 0]      dividen;
+   reg [SIZE-1 : 0] 	     divisor;
+
+   reg [(SIZE*2)-1 : 0]      quotient;
+   reg [(SIZE*2)-1 : 0]      buffer;
+   reg 			     out_valid;
+   
+   reg [(SIZE*2)-1 : 0]      new_divisor;
+   reg [(SIZE*2)-1 : 0]      prev_divisor;
+   reg [1:0] 		     state = 0;
+
+   wire 		     input_rdy = input_dividen_tvalid & input_divisor_tvalid;
+
+   assign output_tdata = quotient;
+   assign output_tvalid = out_valid;
+      
+   always @(posedge clk)
+     begin
+	if(rst) begin
+	   dividen <= 0;
+	   divisor <= 0;
+	   state <= 2'b00;
+	   out_valid <= 0;
+	   quotient <= 1;
+	   buffer <= 0;
+	end else begin
+	   if(state == 2'b00) begin
+	      if(input_rdy) begin
+		 dividen <= input_dividen_tdata;
+		 divisor <= input_divisor_tdata;
+	      
+		 prev_divisor <= input_divisor_tdata;
+		 new_divisor <= input_divisor_tdata;
+
+		 quotient <= 1;
+		 buffer <= 1;
+	      
+		 state <= 2'b01;
+	      end
+	   end
+	   else if(state == 2'b01) begin
+	      if(new_divisor < dividen) begin
+		 prev_divisor <= new_divisor;	      
+		 new_divisor <= prev_divisor << 1;
+		 quotient <= buffer;
+		 buffer <= quotient << 1;
+	      end else state <= 2'b10;
+	      
+	   end else if(state == 2'b10) begin
+	      dividen <= dividen - prev_divisor;
+	      buffer <= quotient;
+	      
+	      state <= 2'b11;
+	   end else if(state == 2'b11) begin
+	      if(dividen > divisor) begin
+		 if(prev_divisor >= divisor) begin
+		    if(dividen > prev_divisor) begin
+		       dividen <= dividen - prev_divisor;
+		       quotient <= quotient + buffer;
+		    end
+		    else begin
+		       prev_divisor <= prev_divisor >> 1;
+		       buffer <= buffer >> 1;
+		    end
+		 end
+	      end else begin
+		 out_valid <= 1;
+	      end
+	   end
+	end
+     end
+
+endmodule
---
+++ division_tb.v
@@ -0,0 +1,65 @@
+`timescale 1ns / 1ps
+
+module division_tb #(parameter SIZE = 128);
+
+   // Inputs
+   reg clk;
+   reg rst;
+   reg [SIZE-1 : 0] dividen;
+   reg 		    dividen_tvalid;
+   reg [(SIZE/2)-1 : 0] divisor;
+   reg 			divisor_tvalid;
+   reg 			output_tready;
+
+   // Outputs
+   wire 		dividen_tready;
+   wire 		divisor_tready;
+   wire [(SIZE)-1 : 0] 	output_tdata;
+   wire 		 output_tvalid;
+
+   // Instantiate the Unit Under Test (UUT)
+   division uut(
+		.clk(clk), 
+		.rst(rst), 
+		.input_dividen_tdata(dividen), 
+		.input_dividen_tvalid(dividen_tvalid), 
+		.input_dividen_tready(dividen_tready), 
+		.input_divisor_tdata(divisor), 
+		.input_divisor_tvalid(divisor_tvalid), 
+		.input_divisor_tready(divisor_tready), 
+		.output_tdata(output_tdata), 
+		.output_tvalid(output_tvalid), 
+		.output_tready(output_tready)
+      
+		);
+   
+   initial begin
+      clk = 0;
+      forever begin
+	 #10 clk = ~clk;
+      end
+   end
+
+   initial begin
+      // Initialize Inputs
+      rst = 0;
+      //      dividen = 128'd3068845272377378551;
+      dividen = 128'd20610496227029426887129;
+//      dividen = 128'd234095823;
+      dividen_tvalid = 1;
+      divisor = 64'd69814;
+      divisor_tvalid = 1;
+      output_tready = 0;
+      #5;
+      output_tready = 1;	
+
+      
+      // Wait 100 ns for global reset to finish
+      #200;
+      
+      // Add stimulus here
+
+   end
+   
+endmodule
+
